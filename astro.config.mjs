7d7c7e9bfb30d9faa0bf4e63a96537363e59295c
ALI.Z <ali.zaabat@mousquetaires.com>
2024-12-18 00:28:19 +0100

    update element

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://Ali0908.github.io',
+  base: 'dev-portfolio-main',
+  trailingSlash: "always",
   integrations: [tailwind()],
 });
